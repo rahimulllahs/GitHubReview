2a42aa79e5d5fb83ac91f8a5b2bf0cf0adb47b81
rahimulllahs <117786426+rahimulllahs@users.noreply.github.com>
2023-05-31 11:16:27 -0400

    Update ReviewClass.java
    main method and review method removed

--- src/GitReview/ReviewClass.java
+++ src/GitReview/ReviewClass.java
@@ -2,11 +2,4 @@ package GitReview;
 
 public class ReviewClass {
 	
-	public static void main(String[] args) {
-		
-	}
-	public void test () {
-		System.out.println("This is the final review of gitHub session");
-	}
-
-}
+	
